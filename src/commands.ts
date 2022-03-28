3116072c4fdd453ee04e11246b76504d4061001d
nullnyat <nullnyat@outlook.com>
2022-03-28 18:15:39 +0900

    add: タイマーを強化！！

--- src/commands.ts
+++ src/commands.ts
@@ -3,19 +3,27 @@ import Command from './utils/command-gen'
 const commands: readonly Command<string[] | RegExp>[] = [
 
 	new Command(['ping'], async message => {
-		await message.channel.send('Pong!')
+		await message.channel.send('<:bibibi_nullcat:951930336231702548>')
 	}),
 
-	new Command(/([\d.]+)分タイマー/, async (message, match) => {
-		const minutes = Number(match[1])
+	new Command(/(([\d.]+)(時間))?(([\d.]+)(分))?(([\d.]+)(秒))?タイマー/, async (message, match) => {
+		let time = 0
 
-		console.log('A timer set: waiting for %d minutes.', minutes)
-		await message.channel.send(`わかった！${minutes}分後にお知らせするね！`)
+		const hours = 1000 * 60 * 60 * Number(match[2]) || 0
+		const minutes = 1000 * 60 * Number(match[5]) || 0
+		const seconds = 1000 * Number(match[8]) || 0
+
+		time += hours + minutes + seconds
+
+		console.log('A timer set: waiting for %d milliseconds.', time)
+		await message.channel.send('わかった！時間が経ったらお知らせするね！')
 
 		setTimeout(async () => {
-			console.log('Timer expired: %d minutes passed.', minutes)
-			await message.channel.send('時間が経ったよ！！')
-		}, 1000 * 60 * minutes)
+			console.log('Timer expired: %d milliseconds passed.', time)
+
+			const author = message.author
+			await message.channel.send(`${author} 時間が経ったよ！！`)
+		}, time)
 	}),
 
 	new Command(['うっせぇ', 'うっせえ', 'うるせぇ', 'うるせえ', 'うるさい'], async message => {
